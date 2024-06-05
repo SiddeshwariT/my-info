94d977093c23e581e6969485737d261ee21aabe4
Siddeshwari Tamgond <140934504+SiddeshwariT@users.noreply.github.com>
2024-06-05 15:01:56 +0530

    Add files via upload

---
+++ streamlit.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+st.write('Good morning! My name is Sid, I am studying MBA in Business analytics with a focus on Data science')
+
+st.text('Good morning! My name is Sid, I am studying MBA in Business analytics with a focus on Data science')
